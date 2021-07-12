a9995063c260e0a7018d4f29eee7ec2cad742dd1
Yoga Pranata <ahmadzaylani001@gmail.com>
2021-07-12 09:32:19 +0700

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,6 +1,6 @@
 class Translation(object):
     START_TEXT = """Hi Bro, This Is Telegrams Yet Another Sassiest Downloader com Renamer Bot Hit /help to know how to use me
-© @RedbullFed"""
+© @Yoga_CIC"""
     RENAME_403_ERR = "🤣🤣 Bye Bye... My Dev Restricted You From here"
     ABS_TEXT = "Go Away Stupid 🤦‍♀️."
     # UPGRADE_TEXT = "😅 Ok Bie"
@@ -16,7 +16,7 @@ FEATURES:
 👉 Upload as file from any HTTP link, with custom thumbnail support.
 👉 Get Low Speed Direct Download Link of any Telegram file.
 
----@redbullfed----"""
+---@Yoga_CIC----"""
 
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
@@ -26,17 +26,17 @@ URL | filename | username | password"""
     UPLOAD_START = "Getting Closer to you... Uploading"
     RCHD_BOT_API_LIMIT = "Are You Kidding me (50MB) is your Allowed Limit, Neverthless, trying to upload."
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations 🙇‍♀️."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "Please Join Our Movie Group 😌 @movieworldkdy"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nPlease Join My Music Group @mwksongs 😌."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "Please Join Our Hentai Group 😌 @NekoPoiHNTch"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nPlease Join My Bots Channel @SpreadNetworks 😌."
     NOT_AUTH_USER_TEXT = "You Aren't An Approved User Please must Upgrade the plan /upgrade."
-    NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/redbullfed'>MwK Tech Support Group</a>"
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/YBotsSupport'>Support Group</a>"
     SAVED_CUSTOM_THUMB_NAIL = "Custom thumbnail saved. This image will be used in the video / file."
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail Deleted succesfully."
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
     CUSTOM_CAPTION_UL_FILE = " "
     NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
-    NO_VOID_FORMAT_FOUND = "something is wrong with the URL you gave me 🤦‍♀️. If you think this could be a bug please report on https://github.com/shamilhabeebnelli/TG-MegaBot/issues OR @redbullfed\n<b>YouTubeDL</b> said: {}"
+    NO_VOID_FORMAT_FOUND = "something is wrong with the URL you gave me 🤦‍♀️. If you think this could be a bug please report on https://t.me/OwnerCH_Bot OR @Yoga_CIC\n<b>YouTubeDL</b> said: {}"
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     CURENT_PLAN_DETAILS = """Current plan details
 --------
@@ -53,14 +53,14 @@ Expires on: {}"""
    Video  - Give File as video without Screenshots
    DFile  - Give File without Screenshots
    
-<b>👉 Create own Clone Bot :</b> 👉 <a href="https://youtu.be/QkAkSLBgoYw">Diploy</a>
+<b>👉 Create own Clone Bot :</b> 👉 <a href="https://nothing.com">Deploy</a>
 --------
 Send /me to know current plan details"""
     REPLY_TO_DOC_GET_LINK = "Reply to a Telegram media to get High Speed Direct Download Link 😌"
     REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert 😌"
     REPLY_TO_DOC_FOR_SCSS = "Reply to a Telegram media to get screenshots 🤦‍♀️"
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to /rename with custom thumbnail support 🤦‍♀️"
-    AFTER_GET_DL_LINK = "Direct Link <a href='{}'>Generated</a> valid for {} days.\nPowered By @redbullfed"
+    AFTER_GET_DL_LINK = "Direct Link <a href='{}'>Generated</a> valid for {} days.\nPowered By @SpreadNetworks"
     FF_MPEG_RO_BOT_RE_SURRECT_ED = """Syntax: /trim HH:MM:SS [HH:MM:SS]"""
     FF_MPEG_RO_BOT_STEP_TWO_TO_ONE = "First send /downloadmedia to any media so that it can be downloaded to my local. \nSend /storageinfo to know the media, that is currently downloaded. 🤷‍♀️"
     FF_MPEG_RO_BOT_STOR_AGE_INFO = "Video Duration: {}\nSend /clearffmpegmedia to delete this media, from my storage.\nSend /trim HH:MM:SS [HH:MM:SS] to cu[l]t a small photo / video, from the above media."
@@ -71,14 +71,14 @@ Send /me to know current plan details"""
     ERR_ONLY_TWO_MEDIA_IN_ALBUM = "Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album 🤦‍♀️."
     INVALID_UPLOAD_BOT_URL_FORMAT = "URL format is incorrect. make sure your url starts with either http:// or https://. You can set custom file name using the format link | file_name.extension"
     ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction 🤷‍♀️"
-    FF_MPEG_RO_BOT_AD_VER_TISE_MENT = "https://telegram.dog/mwklinks"
+    FF_MPEG_RO_BOT_AD_VER_TISE_MENT = "https://telegram.dog/SpreadNetworks"
     EXTRACT_ZIP_INTRO_ONE = "Send a compressed file first, Then reply /unzip command to the file."
     EXTRACT_ZIP_INTRO_THREE = "Analyzing received file. ⚠️ This might take some time. Please be patient. "
     UNZIP_SUPPORTED_EXTENSIONS = ("zip", "rar")
-    EXTRACT_ZIP_ERRS_OCCURED = "Sorry. Errors occurred while processing compressed file. Please check everything again twice, and if the issue persists, report this to <a href='https://telegram.dog/redbullfed'>MwK Tech Support Group</a>"
+    EXTRACT_ZIP_ERRS_OCCURED = "Sorry. Errors occurred while processing compressed file. Please check everything again twice, and if the issue persists, report this to <a href='https://telegram.dog/YBotsSupport'>Support Group</a>"
     EXTRACT_ZIP_STEP_TWO = """Select file_name to upload from the below options 😌.
 You can use /rename command after receiving file to rename it with custom thumbnail support."""
-    GET_LINK_ERRS_OCCURED = "Sorry the following Errors occurred: \n{}\nPlease check everything again twice, and if the issue persists, report this to <a href='https://telegram.dog/redbullfed'>Mwk Tech Support Group</a>"
+    GET_LINK_ERRS_OCCURED = "Sorry the following Errors occurred: \n{}\nPlease check everything again twice, and if the issue persists, report this to <a href='https://telegram.dog/YBotsSupport'>Support Group</a>"
     CANCEL_STR = "Process Cancelled 🙇‍♀️"
     ZIP_UPLOADED_STR = "Uploaded {} files in {} seconds"
     FREE_USER_LIMIT_Q_SZE = """Cannot Process 😔.
@@ -88,4 +88,4 @@ Free users only 1 request per 30 minutes.
     G_DRIVE_SETUP_IN_VALID_FORMAT = "Send `/gsetup <YOUR CODE>`"
     G_DRIVE_SETUP_COMPLETE = "Logged In."
     SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
-    FED_UP_WITH_CRAP = "This bot is no longer leeching links for free users. @TG_MegaBot is open source, and you can deploy your own telegram upload by clicking on the links, available in <a href='https://github.com/shamilhabeebnelli/TG-MegaBot>GitHub README</a>. or, Better /upgrade the subscription to continue using this bot."
+    FED_UP_WITH_CRAP = "This bot is no longer leeching links for free users. @xUploadUrlBot is open source, and you can deploy your own telegram upload by clicking on the links, available in <a href='https://nothing.com>GitHub README</a>. or, Better /upgrade the subscription to continue using this bot."
